147072472aa186a9ea348372a1bf9a7a9bdb1296
Anoop Anthony <anoop.anthony@gmail.com>
2020-01-25 21:53:06 -0500

    adding an adjacency list map code from geekNgeeks

--- outerMain/Source.cpp
+++ outerMain/Source.cpp
@@ -1,10 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <iostream>
+#include <vector>
 
-//using namespace std;
+using std::cout;
+using std::cin;
+using std::vector;
 
-int main() {
-	std::cout << "oh shit";
+// A utility function to add an edge in an 
+// undirected graph. 
+void addEdge(vector<int> adj[], int u, int v)
+{
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+}
+
+// A utility function to print the adjacency list 
+// representation of graph 
+void printGraph(vector<int> adj[], int V)
+{
+	for (int v = 0; v < V; ++v)
+	{
+		cout << "\n Adjacency list of vertex "
+			<< v << "\n head ";
+		for (auto x : adj[v])
+			cout << "-> " << x;
+		printf("\n");
+	}
+}
+
+int main()
+{
+	cout << "Hi New Haven!\n";
+
+	int V = 5;
+	vector<int> adj[5];
+	addEdge(adj, 0, 1);
+	addEdge(adj, 0, 4);
+	addEdge(adj, 1, 2);
+	addEdge(adj, 1, 3);
+	addEdge(adj, 1, 4);
+	addEdge(adj, 2, 3);
+	addEdge(adj, 3, 4);
+	printGraph(adj, V);
+
 	system("pause");
+
+	return 0;
 }
