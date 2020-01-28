fe625ce4025a429aa4e2b219b75aff59da2d87f1
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-01-28 13:09:51 -0500

    Added dummy implementations for declared functions

--- outerMain/Graph.cpp
+++ outerMain/Graph.cpp
@@ -1,2 +1,18 @@
 #include "Graph.h"
 
+Graph::Graph() {
+}
+
+Node * Graph::addVertex() {
+	return nullptr;
+}
+
+void Graph::addEdge(Node * u, Node * v) {
+}
+
+void Graph::setResource(Resource * r) {
+}
+
+int Graph::search() {
+	return 0;
+}
