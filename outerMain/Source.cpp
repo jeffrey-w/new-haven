87df4a8f03364c2c6249d24679bac18750a8cec7
Dan <dan.raileanu@gmail.com>
2020-02-08 14:58:33 -0500

    removed graph test from source

--- outerMain/Source.cpp
+++ outerMain/Source.cpp
@@ -16,37 +16,7 @@ using std::vector;
 int main()
 {
     cout << "Hi New Haven!\n";
-    //Graph* g=new Graph();
-    //for(int i=0; i<4; i++){
-    //    for(int j=0; j<4; j++) {
-    //        pair<int, int>* p = new pair<int,int>(i, j);
-    //        Node* newNode=new Node(p);
-    //        HarvestTile* ht=new HarvestTile();
-    //        newNode->setObject(ht);
-    //        g->addNode(p, newNode);
-
-        }
-    }
-    for(int i=0; i<3; i++) {
-        for (int j = 0; j < 4; j++) {
-            pair<int, int> *p1 = new pair<int, int>(i, j);
-            pair<int, int> *p2 = new pair<int, int>(i + 1, j);
-
-            g->addEdge(p1, p2);
-        }
-    }
-    for(int i=0; i<4; i++) {
-        for (int j = 0; j < 3; j++) {
-            pair<int, int> *p1 = new pair<int, int>(i, j);
-            pair<int, int> *p2 = new pair<int, int>(i, j+1);
-
-            g->addEdge(p1, p2);
-        }
-    }
-
-
-    g->display();
-
+    
     // TEST GBMap
     GBMap gbmap(2);
     cout << gbmap.search({ 0, 0 }) << "\n";
