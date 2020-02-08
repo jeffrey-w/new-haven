5b66a29a737598edd38c81377e0b538b4117c566
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-07 23:31:34 -0500

    removed getNumPlayers(); refactored load()

--- outerMain/GBMapLoader.cpp
+++ outerMain/GBMapLoader.cpp
@@ -12,7 +12,6 @@ using std::vector;
 GBMapLoader::GBMapLoader(string path) {
 	ifstream file;
 	nodes = new map<pair<int, int>, HarvestTile*>();
-	edges = new vector<pair<pair<int, int>, pair<int, int>>>();
 	try {
 		file.open(path);
 		read(&file);
@@ -21,44 +20,24 @@ GBMapLoader::GBMapLoader(string path) {
 		std::cerr << e.what() << std::endl;
 		std::cerr << "Error code: " << e.code() << std::endl;
 		nodes->clear();
-		edges->clear();
 	}
 	file.close();
 }
 
 GBMapLoader::~GBMapLoader() {
 	delete nodes;
-	delete edges;
 	nodes = nullptr;
-	edges = nullptr;
 }
 
-GBMap GBMapLoader::load() {
-	GBMap map(getNumPlayers());
+GBMap* GBMapLoader::load() {
+	GBMap* map = new GBMap(*numPlayers);
 	for (auto node : *nodes) {
-		map.addNode(node.first);
-		map.setTile(node.first, node.second);
+		map->setTile(node.first, node.second);
 	}
-	for (auto edge : *edges) {
-		map.addEdge(edge.first, edge.second);
-	}
-	map.validate();
+	// TODO validate if required, but this is guaranteed to be "valid"
 	return map;
 }
 
 void GBMapLoader::read(ifstream* file) {
 	// TODO
-}
-
-int GBMapLoader::getNumPlayers() {
-	switch (nodes->size()) { // TODO avoid magic constants
-	case 25:
-		return 2;
-	case 35:
-		return 3;
-	case 49:
-		return 4;
-	default:
-		return 0;
-	}
 }
