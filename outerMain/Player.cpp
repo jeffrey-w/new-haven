99332071156646682629581baf0c1a69eb272621
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-19 08:21:36 -0500

    implemented placeHarvestTile(), drawBuilding(), and drawHarvestTile()

--- outerMain/Player.cpp
+++ outerMain/Player.cpp
@@ -1,30 +1,19 @@
 #include "Player.h"
 
-using std::vector;
-
-int Player::resourceMarkers[]={200,200,200,200};
-
-Player::Player(){
-    villageBoard=new VGMap();
-    harvestTiles=new vector<HarvestTile*>;
-    buildings=new vector<Building*>;
-    gatherFacility=new GatherFacility;
-    buildFacility=new BuildFacility(villageBoard);
+void Player::placeHarvestTile(HarvestTile* tile, GBMap* map, std::pair<int, int> square) {
+	map->setSquare(square, tile);
 }
 
-
-
-bool Player::buildVillage(int x, int y, Building* building) {//TODO make sure placed next to similar color
-    if(building->getFaceUp() && building->getNumber()!=y){return false;}
-    int buildingResource=building->getColour();
-    if(resourceMarkers[buildingResource]<y){return false;}//not enough resources to build
-    if(villageBoard->placeBuilding(x,y,building)){
-        resourceMarkers[buildingResource]=resourceMarkers[buildingResource]-y;
-        return true;
-    }
-    return false;
+Building* Player::drawBuilding() {
+	if (true) { // TODO !harvestTiles.empty()
+		return buildings->giveBuilding();
+	}
+	throw new std::exception(); // TODO need richer exception type
 }
 
-int Player::calculateScore() {
-   return buildFacility->calculateVillageScore();
+HarvestTile* Player::drawHarvestTile() {
+	if (true) {
+		return harvestTiles->giveHarvestTile();
+	}
+	throw new std::exception(); // TODO need richer exception type
 }
