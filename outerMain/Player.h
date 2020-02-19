0d4f48e765e01399a78585ec2a7be5d81c31324e
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-19 08:23:00 -0500

    modified function prototypes; modified member varaibles

--- outerMain/Player.h
+++ outerMain/Player.h
@@ -1,35 +1,34 @@
 #pragma once
 
-#include "VGMap.h"
-#include "HarvestTile.h"
 #include "Building.h"
+#include "Deck.h"
+#include "GBMap.h"
+#include "HarvestTile.h"
 #include "Resource.h"
 #include "ScoringFacilities.h"
+#include "VGMap.h"
 
 
 
 class Player {
-private:
-    //VGMap* villageBoard;
-    std::vector<HarvestTile*>* harvestTiles;
-    std::vector<Building*>* buildings;
-    GatherFacility* gatherFacility;
-    BuildFacility* buildFacility;
-    static int resourceMarkers[];
-
 
 public:
+
     Player();
-    bool placeHarvestTile(int,int, HarvestTile*);
+    void placeHarvestTile(HarvestTile*, GBMap*, std::pair<int, int>);
     Building* drawBuilding();
     HarvestTile* drawHarvestTile();
-    //int* resourceTracker();//TODO find use for it since no needed right now
-    bool buildVillage(int,int, Building*);
-    //ResourceMarkers* calculateResources() TODO
-    int calculateScore();
-    VGMap* villageBoard;//TODO make private later, but now needed to print VGMap from main
+    void resourceTracker(); // TODO what is this for?
+    void buildVillage(VGMap*, ScoringFacility*);
+    void calculateResources(GBMap*, ResourceMarkers*);
 
-
+private:
+    
+    VGMap* villageBoard;
+    Deck* harvestTiles;
+    Deck* buildings;
+    GatherFacility* gatherFacility;
+    BuildFacility* buildFacility;
 
 };
 
