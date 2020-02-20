1341da700903d3ccbb4b4f79279621c172022400
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-20 08:55:31 -0500

    added incrementBy() to ScoringFacility; added isCalculated() and setCalculated() to GatherFacility

--- outerMain/ScoringFacilities.h
+++ outerMain/ScoringFacilities.h
@@ -1,5 +1,11 @@
 #pragma once
+
 class ScoringFacility {
+
+public:
+
+	virtual void incrementBy(int, int) = 0;
+
 };
 
 class BuildFacility : public ScoringFacility {
@@ -7,6 +13,11 @@ class BuildFacility : public ScoringFacility {
 };
 
 class GatherFacility : public ScoringFacility {
+
+public:
+
+	bool isCalculated(int);
+	void setCalculated(int);
 
 };
 
