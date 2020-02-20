19a3693a25791e0208487d926649eefe8436a85f
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-20 17:11:48 -0500

    inherit from AbstractMap

--- outerMain/VGMap.h
+++ outerMain/VGMap.h
@@ -1,51 +1,18 @@
 #pragma once
 
-#include <map>
-#include <set>
-#include <vector>
-
+#include "AbstractMap.h"
 #include "Building.h"
 
-class VGMap {
+class VGMap : public AbstractMap {
 
-    static constexpr int HEIGHT = 6, WIDTH = 5;
+	static constexpr int HEIGHT = 6, WIDTH = 5;
 
 public:
 
-    VGMap();
-    VGMap(VGMap&) = delete; // Supress copy constructor
-    ~VGMap();
-    void setCicle(std::pair<int, int>, Building*);
-
-private:
-
-    class Node {
-
-    public:
-
-        static constexpr int WHITE = 0, GRAY = 1, BLACK = 2;
-
-        Building* building;
-        std::set<Node*>* adjacents;
-
-        // Search Attributes.
-        int* color;
-        int* distance;
-        Node* prev;
-
-        Node();
-        Node(Node&);
-        void init(Building*, Building*, std::set<Node*>*);
-    };
-
-    std::map<std::pair<int,int>, Node*>* nodes;
-
-    void build();
-    void addNode(std::pair<int, int>);
-    void addEdge(std::pair<int, int>, std::pair<int, int>);
-    Node* nodeAt(std::pair<int, int>);
-    std::pair<int, int> validateCoordinate(std::pair<int, int>);
-    int search(Node*);
-    void resetSearchAttributes(Building*);
+	VGMap();
+	VGMap(VGMap&) = delete; // TODO supress copy constructor?
+	~VGMap();
+	int costOf(std::pair<int, int>);
+	void setCircle(Building*, std::pair<int, int>);
 
 };
