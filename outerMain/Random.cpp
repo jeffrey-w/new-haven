3d2264683c03617037533481440709acab10b516
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-22 00:59:40 -0500

    minor changes

--- outerMain/Random.cpp
+++ outerMain/Random.cpp
@@ -5,7 +5,7 @@
 using std::chrono::system_clock;
 using std::minstd_rand;
 
-minstd_rand Random::generator(minstd_rand(system_clock::now().time_since_epoch().count()));
+minstd_rand Random::generator(system_clock::now().time_since_epoch().count());
 
 int Random::next(int origin, int bound) {
 	std::uniform_int_distribution<int> dist(origin, bound);
