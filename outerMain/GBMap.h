7185e5671805a07288ded9b266742f2501291a93
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-21 22:54:57 -0500

    added comments

--- outerMain/GBMap.h
+++ outerMain/GBMap.h
@@ -6,6 +6,7 @@
 #include "TokenGraph.h"
 #include "ScoringFacilities.h"
 
+// The common board onto which HarvestTiles are placed.
 class GBMap {
 
 	static constexpr int DEFAULT_NUM_PLAYERS = 2;
@@ -13,11 +14,17 @@ class GBMap {
 
 public:
 
+	// Constructs a new GBMap object.
 	GBMap();
+	// Constructs a new GBMap object for the specified number of players. Throws an exception if the specified number of players is not between two and four inclusive.
 	GBMap(int);
-	GBMap(GBMap&) = delete; // Supress copy constructor.
+	// Supress copy constructor. Recursively defined data structures require identity semantics.
+	GBMap(GBMap&) = delete;
+	// Destroys this GBMap.
 	~GBMap();
+	// Places the specified HarvestTile onto the specified square of this GBMap. Throws an exception if the specified square is not on this GBMap or if it is already occupied.
 	void setSquare(HarvestTile*, std::pair<int, int>);
+	// Counts and records on the specigied GatherFacility the number of connected resources of each type on this GBMap. Throws an exception if this GBMap is empty.
 	void calculateResources(GatherFacility*);
 
 private:
