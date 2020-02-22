5a8af5fa0fea7022d6ec79c8af5745a89ad208a0
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-22 08:31:39 -0500

    edited comments

--- outerMain/GBMap.cpp
+++ outerMain/GBMap.cpp
@@ -30,7 +30,7 @@ void GBMap::setSquare(HarvestTile* tile, pair<int, int> square) { // TODO need t
 
 void GBMap::calculateResources(pair<int, int> from, GatherFacility* resources) {
 	for (auto& coordinate : coordinatesOf(from)) {
-		if (!graph->isBlack(coordinate)) {
+		if (!graph->isBlack(coordinate)) { // coordinate has previously been reached by another search.
 			int amount = graph->search(coordinate);
 			int type = graph->tokenAt(coordinate)->getType();
 			resources->incrementBy(type, amount);
