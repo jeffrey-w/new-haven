5a4583b1fa438249454f0bb21a70f0ba2566413c
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-21 21:05:21 -0500

    added implementation

--- outerMain/Building.cpp
+++ outerMain/Building.cpp
@@ -1,1 +1,9 @@
 #include "Building.h"
+
+BuildingToken* Building::next() {
+	if (*spent) {
+		throw new std::exception();
+	}
+	*spent = true;
+	return token;
+}
--- outerMain/Building.h
+++ outerMain/Building.h
@@ -1,10 +1,20 @@
 #pragma once
 
 #include "AbstractPiece.h"
+#include "BuildingToken.h"
 
 class Building : public AbstractPiece {
 
-	
+public:
+
+	Building();
+	Building(Building&);
+	~Building();
+	BuildingToken* next();
+
+private:
+
+	BuildingToken* token;
 
 };
 
