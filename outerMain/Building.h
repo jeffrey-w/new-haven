34fa520d8f76b857904f82bbc9bf2614ce54daaa
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-21 22:51:31 -0500

    added comments

--- outerMain/Building.h
+++ outerMain/Building.h
@@ -5,14 +5,20 @@
 #include "AbstractPiece.h"
 #include "BuildingToken.h"
 
+// The Piece that belong to BuildingDecks and that occupy VGMaps.
 class Building : public AbstractPiece {
 
 public:
 
+	// Constructs a new Building object.
 	Building();
+	// Constructs a new Building object with the same attributes as the specified Building.
 	Building(Building&);
+	// Destroys this Building.
 	~Building();
-	BuildingToken* next();
+	// Returns the BuildingToken that constitutes this Building. Throws an exception if this Building has already been tokenized.
+	BuildingToken* tokenize();
+	// Writes this Building to the standard output stream.
 	void display() const;
 
 private:
