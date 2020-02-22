71c500b64d4267a2525067c9180e94dabc70be63
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-21 20:11:28 -0500

    implemented calculateResources()

--- outerMain/Player.cpp
+++ outerMain/Player.cpp
@@ -6,10 +6,14 @@ void Player::placeHarvestTile(HarvestTile* tile, GBMap* map, pair<int, int> squa
 	map->setSquare(tile, square);
 }
 
-Building* Player::drawBuilding(BuildingDeck* deck) {
+BuildingToken* Player::drawBuilding(BuildingDeck* deck) {
 	return deck->drawAs(); // TODO document exception
 }
 
 HarvestTile* Player::drawHarvestTile(HarvestTileDeck* deck) {
 	return deck->drawAs(); // TODO document exception
+}
+
+void Player::calculateResources(GBMap* map) {
+	map->calculateResources(gatherFacility);
 }
