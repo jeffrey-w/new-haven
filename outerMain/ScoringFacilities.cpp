d27555cb58b483ab4f12485798708f6fc858da51
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-22 08:32:46 -0500

    added implementation for GatherFacility

--- outerMain/ScoringFacilities.cpp
+++ outerMain/ScoringFacilities.cpp
@@ -1,1 +1,23 @@
 #include "ScoringFacilities.h"
+
+using std::map;
+
+GatherFacility::GatherFacility() {
+	count = new map<int, int>();
+	for (int i = 0; i < 4; i++) { // TODO avoid magic constants
+		(*count)[i] = 0;
+	}
+}
+
+GatherFacility::GatherFacility(GatherFacility& other) {
+	count = new map<int, int>(*other.count);
+}
+
+GatherFacility::~GatherFacility() {
+	delete count;
+}
+
+void GatherFacility::incrementBy(int type, int amount) { // TODO validate type
+	int prior = (*count)[type];
+	(*count)[type] = prior + amount;
+}
