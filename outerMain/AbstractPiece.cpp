71949f631d7b1e1dba2cc5092e59b85ce2d572db
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-21 21:04:50 -0500

    added implementation

--- outerMain/AbstractPiece.cpp
+++ outerMain/AbstractPiece.cpp
@@ -1,1 +1,13 @@
 #include "AbstractPiece.h"
+
+AbstractPiece::AbstractPiece() {
+	spent = new bool(false);
+}
+
+AbstractPiece::AbstractPiece(AbstractPiece& other) {
+	spent = new bool(*other.spent);
+}
+
+AbstractPiece::~AbstractPiece() {
+	delete spent;
+}
--- outerMain/AbstractPiece.h
+++ outerMain/AbstractPiece.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include "AbstractToken.h"
+
 class AbstractPiece {
+
+public:
+
+	AbstractPiece();
+	AbstractPiece(AbstractPiece&);
+	~AbstractPiece();
+	virtual AbstractToken* next() = 0;
+	virtual void display() = 0;
+
+protected:
+
+	bool* spent;
 
 };
 
