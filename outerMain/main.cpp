5d1d53e45fd7750ef9a5dc1d988376e08bf69055
Dan <dan.raileanu@gmail.com>
2020-02-22 23:10:30 -0500

    main test cases

--- outerMain/main.cpp
+++ outerMain/main.cpp
@@ -1,9 +1,47 @@
 #include "GBMap.h"
+#include "VGMap.h"
 
 int main() {
-	GBMap map;
-	HarvestTile tile1;
-	map.setSquare(&tile1, { 0, 0 });
-	map.display();
+
+    //TEST PART 1 (GBMap)
+    GBMap gmap;
+    HarvestTile ht1;
+    HarvestTile ht2;
+    HarvestTile ht3;
+    gmap.setSquare(&ht1, {0,0});
+    gmap.setSquare(&ht2, {1,0});
+    gmap.setSquare(&ht3, {0,1});
+    gmap.display();
+
+    //TEST PART 2 (VGmap)
+    VGMap vmap;
+    Building b1;
+    Building b2;
+    Building b3;
+    vmap.setCircle(&b1, {0,0});
+    vmap.setCircle(&b2, {1,0});
+    vmap.setCircle(&b3, {0,1});
+    vmap.display();
+
+
+
+    //TEST PART 3 (Map loaders)
+
+
+
+
+    //TEST PART 4 (Player)
+
+
+
+    //TEST PART 5 (Deck)
+
+
+
+
+    //TEST PART 6 (Scoring facilities)
+
+
+
 	return 0;
 }
