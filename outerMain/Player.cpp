6e402adbce2ad32ccd135203ec827d18e7bb3a20
Dan <dan.raileanu@gmail.com>
2020-02-24 17:18:04 -0500

    player can buildVillages and calculateScore

--- outerMain/Player.cpp
+++ outerMain/Player.cpp
@@ -2,8 +2,23 @@
 
 using std::pair;
 
-void Player::placeHarvestTile(HarvestTile* tile, GBMap* map, pair<int, int> square) {
-	map->setSquare(tile, square);
+Player::Player(){
+    villageBoard = new VGMap;
+    gatherFacility = new GatherFacility;
+    buildFacility = new BuildFacility;
+}
+
+Player::Player(const Player& other){
+    //villageBoard=new VGMap(*other.villageBoard);//TODO consider how must behave with VGmap having no copy constructor
+    villageBoard=other.villageBoard; //TODO for now shallow, consider fixing it
+    gatherFacility = new GatherFacility(*other.gatherFacility);
+    buildFacility = new BuildFacility(*other.buildFacility);
+}
+
+Player::~Player(){
+    delete villageBoard;
+    delete gatherFacility;
+    delete buildFacility;
 }
 
 Building* Player::drawBuilding(BuildingDeck* deck) {
@@ -14,6 +29,21 @@ HarvestTile* Player::drawHarvestTile(HarvestTileDeck* deck) {
 	return deck->drawAs(); // TODO document exception
 }
 
+void Player::buildVillage(Building* building, bool faceDown, std::pair<int, int> circle) {
+    villageBoard->setCircle(building, circle);
+    int index=circle.first * villageBoard->WIDTH + circle.second;//calulates associated index in buildFacility
+    buildFacility->markOccupied(index);
+    if(faceDown){buildFacility->markFaceDown(index);}
+}
+
+void Player::placeHarvestTile(HarvestTile* tile, GBMap* map, pair<int, int> square) {
+    map->setSquare(tile, square);
+}
+
 void Player::calculateResources(GBMap* map, pair<int, int> square) {
 	map->calculateResources(square, gatherFacility); // TODO document exception
+}
+
+int Player::calculateScore() {
+    buildFacility->getScore();
 }
--- outerMain/Player.h
+++ outerMain/Player.h
@@ -14,16 +14,20 @@ class Player {
 public:
 
     Player();
+    Player(const Player&);
+    ~Player();
     Building* drawBuilding(BuildingDeck*);
     HarvestTile* drawHarvestTile(HarvestTileDeck*);
-    void buildVillage(Building*, std::pair<int, int>   );
+    void buildVillage(Building*, bool faceDown, std::pair<int, int>   );
     void placeHarvestTile(HarvestTile*, GBMap*, std::pair<int, int>);
     void resourceTracker(); // TODO what is this for?
     void calculateResources(GBMap*, std::pair<int, int>);
+    int calculateScore();
 
+    VGMap* villageBoard;
 private:
     
-    VGMap* villageBoard;
+    //VGMap* villageBoard;
     GatherFacility* gatherFacility; // TODO does this belong here
     BuildFacility* buildFacility; // TODO does this belong here
 
