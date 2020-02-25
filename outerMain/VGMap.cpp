161b804774d7e69ee1b6602d4bcd1d33276ceb24
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-24 23:25:01 -0500

    removed commented out code

--- outerMain/VGMap.cpp
+++ outerMain/VGMap.cpp
@@ -8,24 +8,16 @@ using std::queue;
 using std::cout;
 
 VGMap::VGMap() {
-    graph=TokenGraph::gridOf(HEIGHT, WIDTH);
+    graph = TokenGraph::gridOf(HEIGHT, WIDTH);
 }
 
 VGMap::~VGMap() {
 	delete graph;
 }
 
-void VGMap::setCircle( Building* building, std::pair<int, int> circle) {
+void VGMap::setCircle( Building* building, pair<int, int> circle) {
 	graph->setTokenAt(building->tokenize(), circle);
 }
-
-/*VGMap::Node* VGMap::nodeAt(std::pair<int, int> coordinate) {
-	return nodes->at(validateCoordinate(coordinate));
-}*/
-
-/*std::pair<int, int> VGMap::validateCoordinate(std::pair<int, int> coordinate) {
-	//TODO
-}*/
 
 void VGMap::display() {
     for (int i = 0; i < HEIGHT; i++) {
