360295cdf4777eaffea24875f2f80aa08f26f3c6
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-27 12:47:44 -0500

    added parameterized constuctor; updted references to isSpent to isTokenized(); edited comments

--- outerMain/HarvestTile.h
+++ outerMain/HarvestTile.h
@@ -6,21 +6,30 @@
 #include "ResourceToken.h"
 
 
+// The Piece that occupies HarvestTileDecks and GBMaps.
 class HarvestTile : public AbstractPiece {
 
 	friend class GBMapLoader;
 
 public:
 
+	// The number of resources that a HarvestTile holds.
 	constexpr static int NUM_RESOURCES = 4;
 	
+	// Constructs a new HarvestTile object.
 	HarvestTile();
+	// Constructs a new HarvestTile object with the same resources and configuration as the
+	// specified HarvestTile.
 	HarvestTile(const HarvestTile&);
+	// Destroys this HarvestTile.
 	~HarvestTile();
-	// Rotates ResourceTokens 90 degrees clockwise the specified number of rotations.
+	// Rotates this HarvestTile 90 degrees clockwise the specified number of rotations. Throws an
+	// exception if this HarvestTile as already been tokenized.
 	void rotate(int);
 	// Returns true iff this HarvestTile has been completely tokenized.
-	bool isSpent() const;
+	bool isTokenized() const;
+	// Iteratively returns the ResourceTokens that constitute this HarvestTile. Throws an
+	// exception if this HarvestTile has already been tokenized.
 	ResourceToken* tokenize();
 
 private:
@@ -28,6 +37,8 @@ private:
 	int* current;
 	std::vector<ResourceToken*>* resources;
 
+	// For GBMapLoader.
+	HarvestTile(int);
 	void ensureNotPlaced();
 
 };
