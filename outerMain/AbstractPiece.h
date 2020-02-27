83fea1b9446a63dce0e6a6631a09722c4369e727
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-27 12:38:56 -0500

    changed name of isSpent() to isTokenized(); edited comments

--- outerMain/AbstractPiece.h
+++ outerMain/AbstractPiece.h
@@ -8,8 +8,9 @@ class AbstractPiece {
 public:
 
 	// Returns true iff this AbstractPiece has been completely tokenized.
-	virtual bool isSpent() const = 0;
-	// Iteratively returns the tokens that constitute this AbstractPiece. Throws an exception if this Piece is spent.
+	virtual bool isTokenized() const = 0;
+	// Iteratively returns the tokens that constitute this AbstractPiece. Throws an exception if
+	// this Piece has already been tokenized.
 	virtual AbstractToken* tokenize() = 0;
 
 };
