ab0d75361e504721e110bfa0c195b548ef9dc00c
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-27 12:45:15 -0500

    added errorMessage(); edited comments

--- outerMain/GBMapLoader.h
+++ outerMain/GBMapLoader.h
@@ -3,14 +3,22 @@
 #include "GBMap.h"
 #include "Scanner.h"
 
+// A utility to read a serialized GBMap and return it as an object.
 class GBMapLoader {
 
 public:
 
+	// Supress default constructor.
 	GBMapLoader() = delete;
+	// Constructs a new GBMapLoader from the specified path. Throws an exception if the file at the
+	// specified path cannot be opened.
 	GBMapLoader(std::string);
-	GBMapLoader(GBMapLoader&) = delete;
+	// Suppress copy constructor.
+	GBMapLoader(const GBMapLoader&) = delete;
+	// Destroys this GBMapLoader.
 	~GBMapLoader();
+	// Returns a GBMap read from the file specified at the creation of this GBMapLoader. Throws an
+	// exception if the specified file is not properly formatted or if it contains invalid map data.
 	GBMap* load();
 
 private:
@@ -21,5 +29,7 @@ private:
 	HarvestTile* nextTile();
 	ResourceToken* nextToken();
 	std::pair<int, int> nextSquare();
+
+	std::string errorMessage(std::string);
 
 };
