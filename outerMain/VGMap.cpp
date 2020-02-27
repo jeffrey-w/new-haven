60a285e3ec60c945837aee5420ebc1efd238f751
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-27 12:59:09 -0500

    added copy constructor, calculateScore(), countRows(), and countCols(); modified setCircle() and validateCircle()

--- outerMain/VGMap.cpp
+++ outerMain/VGMap.cpp
@@ -1,6 +1,7 @@
-#include <queue>
-#include "VGMap.h"
 #include <iostream>
+#include <queue>
+
+#include "VGMap.h"
 
 using std::array;
 using std::map;
@@ -10,7 +11,18 @@ using std::cout;
 
 VGMap::VGMap() {
     graph = TokenGraph::gridOf(HEIGHT, WIDTH);
-    typePlaced = new std::array<bool,4>();
+    typePlaced = new array<bool,4>();
+}
+
+VGMap::VGMap(const VGMap& other) : VGMap() {
+    for (auto& entry : other.graph->tokens()) {
+        BuildingToken* orig = static_cast<BuildingToken*>(entry.second);
+        BuildingToken* building = orig ? new BuildingToken(*orig) : nullptr;
+        graph->setTokenAt(building, entry.first);
+    }
+    for (int i = 0; i < 4; i++) {
+        (*typePlaced)[i] = (*other.typePlaced)[i];
+    }
 }
 
 VGMap::~VGMap() {
@@ -18,23 +30,82 @@ VGMap::~VGMap() {
 	delete typePlaced;
 }
 
-void VGMap::setCircle(Building* building, std::pair<int, int> circle) {
+void VGMap::setCircle(Building* building, pair<int, int> circle) {
     validateCircle(circle);
-    int buildType=static_cast<int>(building->getType());
-    if((*typePlaced)[buildType] && !(graph->adjacentHolds(circle, buildType))){
-        throw std::logic_error("Building must be placed adjacent to same type");
+    int type = building->getType();
+    if(graph->hasType(type) && !graph->adjacentHolds(circle, type)) {
+        throw std::logic_error("Building must be placed adjacent to same type.");
     }
-    if(building->isFaceUp() && building->getValue()!=HEIGHT-circle.first)
-    {
-        throw std::logic_error("FaceUp buildings must be placed in appropriate row");
+    if(building->isFaceUp() && !valuesMatch(building, circle.first)) {
+        throw std::logic_error("Face up buildings must be placed in appropriate row.");
     }
 	graph->setTokenAt(building->tokenize(), circle);
+    (*typePlaced)[type] = true;
 }
 
-void VGMap::validateCircle(std::pair<int, int> circle) {
-	if(circle.first<0 || circle.first>HEIGHT-1 || circle.second<0 || circle.second>WIDTH-1){
-	    throw std::invalid_argument("Invalid coordinates");
+void VGMap::validateCircle(pair<int, int> circle) {
+    int row = circle.first, col = circle.second;
+	if(row < 0 || row >= HEIGHT || col < 0 || col >= WIDTH) {
+	    throw std::invalid_argument("Cirlce is not on board.");
 	}
+}
+
+bool VGMap::valuesMatch(Building* building, int row) {
+    return building->getValue() == HEIGHT - row;
+}
+
+void VGMap::calculateScore(VillageBuildingScoringFacility* score) {
+    score->incrementBy(countRows() + countCols());
+}
+
+int VGMap::countRows() {
+    int row, col, multiplier, score = 0;
+    BuildingToken* building;
+    for (row = 0; row < HEIGHT; row++) {
+        multiplier = 2;
+        for (col = 0; col < WIDTH; col++) {
+            building = static_cast<BuildingToken*>(graph->tokenAt({ row, col }));
+            if (building) {
+                if (!building->isFaceUp()) {
+                    multiplier = 1;
+                }
+            }
+            else {
+                break;
+            }
+        }
+        if (col == WIDTH) {
+            score += (WIDTH - row) * multiplier;
+        }
+    }
+    return score;
+}
+
+int VGMap::countCols() {
+    int row, col, multiplier, score = 0;
+    BuildingToken* building;
+    for (col = 0; col < WIDTH; col++) {
+        multiplier = 2;
+        for (row = 0; row < HEIGHT; row++) {
+            building = static_cast<BuildingToken*>(graph->tokenAt({ row, col }));
+            if (building) {
+                if (!building->isFaceUp()) {
+                    multiplier = 1;
+                }
+            }
+            else {
+                break;
+            }
+        }
+        if (row == HEIGHT) {
+            /*
+             * Column scores are as follow:
+             * 0:5 | 1:4 | 2:3 | 3:4 | 4:5
+             */
+            score += (HEIGHT - col + (col > 2 ? 2 * (col - 2) : 0)) * multiplier;
+        }
+    }
+    return score;
 }
 
 void VGMap::display() {
