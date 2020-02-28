024d4a325e5fb4b9ecb66ac7692b20f3962bc9e0
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-28 10:28:59 -0500

    complete refactoring

--- outerMain/Resources.cpp
+++ outerMain/Resources.cpp
@@ -1,34 +1,83 @@
+#include <stdexcept>
+
 #include "Resources.h"
 
-AbstractPiece* Deck::draw() {
-	if (empty()) {
-		throw std::exception(); // TODO need richer exception type
-	}
-	AbstractPiece* piece = pieces->top();
-	pieces->pop();
-	return piece;
+using std::stack;
+using std::vector;
+
+Deck::Deck() {
+	pieces = new stack<AbstractPiece*>();
 }
 
-bool Deck::empty() {
+Deck::~Deck() {
+	delete pieces;
+}
+
+bool Deck::empty() const {
 	return pieces->empty();
 }
 
-void Deck::insert(AbstractPiece* piece) {
+void Deck::push(AbstractPiece* piece) {
+	if (!piece) {
+		throw std::invalid_argument("Cannot add the null Piece to a deck.");
+	}
 	pieces->push(piece);
 }
 
-void BuildingDeck::add(Building* building) {
-	insert(building);
+AbstractPiece* Deck::pop() {
+	if (empty) {
+		throw std::runtime_error("Cannot draw from an empty deck.");
+	}
+	AbstractPiece* result = pieces->top();
+	pieces->pop();
+	return result;
 }
 
-Building* BuildingDeck::drawAs() {
-	return static_cast<Building*>(draw());
+std::vector<AbstractPiece*>& Deck::asList() const {
+	stack<AbstractPiece*> s;
+	auto list = new vector<AbstractPiece*>();
+	while (!pieces->empty()) {
+		s.push(pieces->top());
+		list->push_back(pieces->top());
+		pieces->pop();
+	}
+	while (!s.empty()) {
+		pieces->push(s.top());
+		s.pop();
+	}
+	return *list;
+}
+
+BuildingDeck::BuildingDeck() : Deck() {}
+
+BuildingDeck::BuildingDeck(const BuildingDeck& other) : BuildingDeck() {
+	for (auto& piece : other.asList()) {
+		Building* building = static_cast<Building*>(piece);
+		push(new Building(*building));
+	}
+}
+
+void BuildingDeck::add(Building* building) {
+	push(building);
+}
+
+Building* BuildingDeck::draw() {
+	return static_cast<Building*>(pop());
+}
+
+HarvestTileDeck::HarvestTileDeck() : Deck() {}
+
+HarvestTileDeck::HarvestTileDeck(const HarvestTileDeck& other) : HarvestTileDeck() {
+	for (auto piece : other.asList()) {
+		HarvestTile* tile = static_cast<HarvestTile*>(piece);
+		push(new HarvestTile(*tile));
+	}
 }
 
 void HarvestTileDeck::add(HarvestTile* tile) {
-	insert(tile);
+	push(tile);
 }
 
-HarvestTile* HarvestTileDeck::drawAs() {
-	return static_cast<HarvestTile*>(draw());
+HarvestTile* HarvestTileDeck::draw() {
+	return static_cast<HarvestTile*>(pop());
 }
--- outerMain/Resources.h
+++ outerMain/Resources.h
@@ -1,43 +1,82 @@
 #pragma once
 
 #include <stack>
+#include <vector>
 
 #include "AbstractPiece.h"
 #include "Building.h"
 #include "HarvestTile.h"
 
+// The base class from which all Decks (i.e. containers for Pieces) shall be derived.
 class Deck {
 
 public:
-
-	AbstractPiece* draw();
-	bool empty();
+	
+	// Constructs a new Deck object.
+	Deck();
+	// Destroys this Deck.
+	~Deck();
+	// Removes and returns the Piece at the top of this Deck.
+	virtual AbstractPiece* draw() = 0;
+	// Returns true iff this Deck has no pieces in it.
+	bool empty() const;
 
 protected:
 
-	void insert(AbstractPiece*);
-	virtual AbstractPiece* drawAs() = 0;
+	// Puts the specified Piece on the top of this Deck.
+	void push(AbstractPiece*);
+	// Removes the Piece at the top of this Deck.
+	AbstractPiece* pop();
+	// Returns a list view of this Pieces in this Deck.
+	std::vector<AbstractPiece*>& asList() const;
 
 private:
 
 	std::stack<AbstractPiece*>* pieces;
 
+
 };
 
+// The Deck that contains Buildings.
 class BuildingDeck : public Deck {
 
 public:
 
+	// Constructs a new BuildingDeck object.
+	BuildingDeck();
+	// Constructs a new BuildingDeck object that contains copies of the Buildings contained by the
+	// specified BuildingDeck.
+	BuildingDeck(const BuildingDeck&);
+	// Adds the specified Building to this BuildingDeck.
 	void add(Building*);
-	Building* drawAs();
+	// Removes and returns the Building at the top of this BuildingDeck.
+	Building* draw();
 
 };
 
+// The Deck that contains HarvestTiles.
 class HarvestTileDeck : public Deck {
 
 public:
 
+	// Constructs a new HarvestTileDeck object.
+	HarvestTileDeck();
+	// Constructs a new HarvestTileDeck object that contains copies of the HarvestTiles contained
+	// by the specified HarvestTileDeck.
+	HarvestTileDeck(const HarvestTileDeck&);
+	// Adds the specified HarvestTile to this HarvestTileDeck.
 	void add(HarvestTile*);
-	HarvestTile* drawAs();
+	// Removes and returns the HarvestTile at the top of this HarvestTileDeck.
+	HarvestTile* draw();
+
+};
+
+class Hand {
+
+	void exchange();
+
+private:
+
+	vector<HarvestTile*>* tiles;
 
 };
