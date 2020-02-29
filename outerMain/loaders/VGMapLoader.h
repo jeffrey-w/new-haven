e5b7e4aacb5d5f7864fef2f7486c7ddf684e6052
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-29 01:43:40 -0500

    added comments

--- outerMain/loaders/VGMapLoader.h
+++ outerMain/loaders/VGMapLoader.h
@@ -3,14 +3,22 @@
 #include "../maps/VGMap.h"
 #include "../util/Scanner.h"
 
+// A utility to read a serialized VGMap and return it as an object.
 class VGMapLoader {
 
 public:
 
+	// Suppress default constructor.
 	VGMapLoader() = delete;
+	// Constructs a new VGMapLoader from the specified path. Throws an exception if the file at the
+	// specified path cannot be opened.
 	VGMapLoader(std::string);
+	// Suppress copy constructor.
 	VGMapLoader(VGMapLoader&) = delete;
+	// Destroys this VGMapLoader.
 	~VGMapLoader();
+	// Returns a VGMap read from the file specified at the creation of this GBMapLoader. Throws an
+	// exception if the specified file is not properly formatted or if it contains invalid map data.
 	VGMap* load();
 
 private:
