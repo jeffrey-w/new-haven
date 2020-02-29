5c54aab65bbb7ce226833a0f3ff2104761fe05a7
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-29 01:38:33 -0500

    added comments

--- outerMain/maps/VGMap.h
+++ outerMain/maps/VGMap.h
@@ -12,12 +12,23 @@ public:
 
 	static constexpr int HEIGHT = 6, WIDTH = 5;
 
+	// Constructs a new VGMap object.
 	VGMap();
+	// Constructs a new VGMap object with the same configuration as the specified VGMap.
 	VGMap(const VGMap&);
+	// Destroys this VGMap.
 	~VGMap();
+	// Places the specified Building onto the specified circle of this VGMap. Throws an exception
+	// if any of the following conditions hold:
+	// (1) the specified Building is null,
+	// (2) the specified circle is not on this VGMap or it is already occupied,
+	// (3) the specified Building is not being placed adjacent to another of the same type, or
+	// (4) the specified Building is being played face up on a circle that is not the correct value.
 	void setCircle(Building*, std::pair<int, int>);
+	// Counts the villagers that have been attracted to this VGMap.
 	void calculateScore(BuildFacility*);
-    void display();
+	// Writes this VGMap to the standard output stream.
+	void display();
 
 private:
 
