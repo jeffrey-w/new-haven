f3f72a74b1e81ba5217bc23c5886c3161b9e588d
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-01 01:36:51 -0500

    modularized tests for GBMap, VGMap, and loaders

--- outerMain/main.cpp
+++ outerMain/main.cpp
@@ -5,7 +5,9 @@
 #include "maps/GBMap.h"
 #include "maps/VGMap.h"
 #include "Player.h"
-
+#include "test/GBMapDriver.h"
+#include "test/MapLoaderDriver.h"
+#include "test/VGMapDriver.h"
 
 // TODO delete this
 #define ASSERT_THROWS(exception, expression, msg) { \
@@ -20,55 +22,10 @@
 }
 
 int main() {
-
-
-    //TEST PART 1 (GBMap)
-    GBMap gmap;
-    // Connectedness.
-    std::cout << numberOfNodes(gmap) << std::endl;
-    HarvestTile ht1;
-    HarvestTile ht2;
-    HarvestTile ht3;
-    HarvestTile ht4(ht1);
-    HarvestTile ht5(ht2);
-    HarvestTile ht6(ht3);
-    gmap.setSquare(&ht1, {0,0});
-    gmap.setSquare(&ht2, {1,0});
-    gmap.setSquare(&ht3, {0,1});
-    ht4.rotate(1);
-    ht5.rotate(2);
-    ht6.rotate(3);
-    gmap.setSquare(&ht4, {3,3});
-    gmap.setSquare(&ht5, {4,3});
-    gmap.setSquare(&ht6, {3,4});
-    gmap.display();
-    // Invalid map.
-    ASSERT_THROWS(std::invalid_argument, GBMap(5), "ERROR: invalid number of players allowed.");
-
-
-    std::cout<<"================================================================================\n";
-    //TEST PART 2 (VGmap)
-    VGMap vmap;
-    for (int i = 0; i < VGMap::WIDTH; i++) {
-        vmap.setCircle(new Building(BuildingType::FOREST, 1), { 0, i });
-    }
-    for (int i = 1; i < VGMap::HEIGHT; i++) {
-        vmap.setCircle(new Building(BuildingType::MEADOW, 1), { i, 0 });
-    }
-    vmap.display();
-
-
-    std::cout<<"================================================================================\n";
-    //TEST PART 3 (Map loaders)
-    GBMapLoader gloader1("res/gmap.txt");
-    GBMap* gloaded = gloader1.load();
-    gloaded->display();
-    delete gloaded;
-    gloaded = nullptr;
-    GBMapLoader gloader2("res/gmap_bad.txt");
-    ASSERT_THROWS(std::invalid_argument, gloader2.load(), "ERROR: Invalid map read.");
-    GBMapLoader gloader3("res/gmap_bad_format.txt");
-    ASSERT_THROWS(std::runtime_error, gloader3.load(), "ERROR: Read from badly formatted file.")
+    testGBMap();
+    testVGMap();
+    testGBMapLoader();
+    testVGMapLoader();
 
     VGMapLoader vloader1("res/vmap.txt");
     VGMap* vloaded = vloader1.load();
