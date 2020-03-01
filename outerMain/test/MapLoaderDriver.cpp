10d3c7876dc4d732eda9d2777df92ab9cd9c5be6
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-01 18:05:25 -0500

    modified testGBMapLoader() and testVGMapLoader()

--- outerMain/test/MapLoaderDriver.cpp
+++ outerMain/test/MapLoaderDriver.cpp
@@ -9,7 +9,7 @@ void testGBMapLoader() {
 	GBMapLoader syntax("./res/gmap_bad_format.txt");
 	GBMap* loaded = nullptr;
 	std::cout << "Running GBMapLoader tests:" << std::endl;
-	ASSERT_SUCCESS((loaded = valid.load()), "loaded valid GBMap.");
+	ASSERT_SUCCESS((loaded = valid.load()), "Successfully loaded valid GBMap.");
 	ASSERT_THROWS(std::invalid_argument, semantics.load(), "ERROR: loaded invalid GBMap.");
 	ASSERT_THROWS(std::runtime_error, syntax.load(), "ERROR: read invalid file.");
 	std::cout << std::endl;
@@ -22,7 +22,7 @@ void testVGMapLoader() {
 	VGMapLoader syntax("./res/vmap_bad_syntax.txt");
 	VGMap* loaded = nullptr;
 	std::cout << "Running VGMapLoader tests:" << std::endl;
-	ASSERT_SUCCESS((loaded = valid.load()), "loaded valid VGMap.");
+	ASSERT_SUCCESS((loaded = valid.load()), "Successfully loaded valid VGMap.");
 	ASSERT_THROWS(std::invalid_argument, semantics.load(), "ERROR: loaded invalid VGMap.");
 	ASSERT_THROWS(std::runtime_error, syntax.load(), "ERROR: read invalid file.");
 	std::cout << std::endl;
