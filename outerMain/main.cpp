020c7094af28b32a3cce81e169e9ee331b94ff12
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-07 20:52:30 -0500

    add initGBMap()

--- outerMain/main.cpp
+++ outerMain/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
+#include "maps/GBMap.h"
 #include "test/Drivers.h"
+
+static GBMap* initGBMap();
 
 int main() {
     testGBMap();
@@ -10,5 +15,40 @@ int main() {
     testPlayer();
     testResources();
     testScoringFacilities();
+    GBMap* map = initGBMap();
+    map->display();
+    delete map;
 	return 0;
+}
+
+GBMap* initGBMap() {
+    int numPlayers = 0;
+    GBMap* map = nullptr;
+    do {
+    std::cout << "Enter number of players: ";
+    std::string input;
+    std::cin.ignore();
+    std::getline(std::cin, input);
+    std::stringstream(input) >> numPlayers;
+        try {
+            map = new GBMap(numPlayers);
+        } catch (std::invalid_argument & e) {
+            std::cerr << e.what() << std::endl;
+            std::cerr << "Accept default number of players? Y/n: ";
+            switch (std::cin.get()) {
+            case 'Y':
+            case 'y':
+                map = new GBMap();
+                std::cout << "Creating two player game.\n";
+                break;
+            case 'N':
+            case 'n':
+                std::cout << "Okay, try again.\n";
+                break;
+            default:
+                std::cout << "Did not understantd, try again.\n";
+            }
+        }
+    } while (!map);
+    return map;
 }
