30942386a8b8d107b469a428f0a45eee72c0777e
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-17 11:10:07 -0400

    add comments

--- outerMain/Roster.h
+++ outerMain/Roster.h
@@ -5,18 +5,28 @@
 
 #include "Player.h"
 
+// A collection of New Haven Players.
 class Roster {
 
 	static constexpr int TILE_HAND_SIZE = 3;
 
 public:
 
+	// Constructs a new Roster object with the specified number of Players. Throws an exception of
+	// the specified number of Players is less than two.
 	Roster(int);
+	// Constructs a new Roster object with the same attributes as the specified Roster.
 	Roster(const Roster&);
+	// Destroys this Roster.
 	~Roster();
+	// Deals a perscribed number of HarvestTiles and Buildings from the specified Decks to each
+	// Player in this Roster. Throws an exception if either of the specified Decks is null.
 	void deal(Deck<HarvestTile*>*, Deck<Building*>*);
+	// Returns the next Player in this Roster to take a turn.
 	Player* next();
-	void enqueue(Player*); // TODO rename this
+	// Inserts the specified Player back into this Roster after having taken a turn. Throws an
+	// exception if the specified Player does not belong to this Roster.
+	void enqueue(Player*);
 
 
 private:
