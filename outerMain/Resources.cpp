fdef2c80a7e7b7fe880c0ff6a730da99c5c69f64
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-18 11:44:00 -0400

    formatting

--- outerMain/Resources.cpp
+++ outerMain/Resources.cpp
@@ -7,70 +7,6 @@
 using std::vector;
 using ResourceType = ResourceToken::ResourceType;
 
-Deck<HarvestTile*>* harvestTileDeck() {
-	Deck<HarvestTile*>* tiles = new Deck<HarvestTile*>();
-	for (int i = 0; i < TokenGraph::NUM_TYPES; i++) {
-		for (int j = 0; j < TokenGraph::NUM_TYPES; j++) {
-			if (i != j) {
-				// For each resource type i and each type j, add a new tile with three i and one j.
-				tiles->add(new HarvestTile(
-					new ResourceToken(static_cast<ResourceType>(i)),
-					new ResourceToken(static_cast<ResourceType>(i)),
-					new ResourceToken(static_cast<ResourceType>(i)),
-					new ResourceToken(static_cast<ResourceType>(j))
-				));
-				// For each resource type i and each type j, add a new tile with two i and two j.
-				tiles->add(new HarvestTile(
-					new ResourceToken(static_cast<ResourceType>(i)),
-					new ResourceToken(static_cast<ResourceType>(j)),
-					new ResourceToken(static_cast<ResourceType>(i)),
-					new ResourceToken(static_cast<ResourceType>(j))
-				));
-			}
-		}
-	}
-	for (int i = 0; i < TokenGraph::NUM_TYPES; i++) {
-		for (int j = 0; j < TokenGraph::NUM_TYPES; j++) {
-			for (int k = 0; k < TokenGraph::NUM_TYPES; k++) {
-				if (!(i == j || i == k || j == k)) {
-					if (j < k) {
-						// For each resource type i, each type j, and each type k, add a new tile
-						// with two i (on the main diagonal), one j, and one k.
-						tiles->add(new HarvestTile(
-							new ResourceToken(static_cast<ResourceType>(i)),
-							new ResourceToken(static_cast<ResourceType>(j)),
-							new ResourceToken(static_cast<ResourceType>(k)),
-							new ResourceToken(static_cast<ResourceType>(i))
-						));
-					}
-					// For each resource type i, each type j, and each type k, add a new tile
-					// with two i (on the first column), one j, and one k.
-					tiles->add(new HarvestTile(
-						new ResourceToken(static_cast<ResourceType>(i)),
-						new ResourceToken(static_cast<ResourceType>(j)),
-						new ResourceToken(static_cast<ResourceType>(i)),
-						new ResourceToken(static_cast<ResourceType>(k))
-					));
-				}
-			}
-		}
-	}
-	return tiles;
-}
-
-Deck<Building*>* buildingDeck() {
-	Deck<Building*>* buildings = new Deck<Building*>();
-	for (int i = 0; i < VGMap::HEIGHT; i++) {
-		for (int j = 0; j < VGMap::HEIGHT; j++) {
-			for (int k = 0; k < TokenGraph::NUM_TYPES; k++) {
-				BuildingType type = static_cast<BuildingType>(k);
-				buildings->add(new Building(type, i + 1));
-			}
-		}
-	}
-	return buildings;
-}
-
 HarvestTileHand::HarvestTileHand() {
 	one = nullptr;
 	two = nullptr;
@@ -296,4 +232,69 @@ std::ostream& operator<<(std::ostream& stream, const BuildingPool& buildings) {
 		}
 	}
 	return stream << '\n';
+}
+
+Deck<HarvestTile*>* harvestTileDeck() {
+	int numTypes = TokenGraph::NUM_TYPES;
+	Deck<HarvestTile*>* tiles = new Deck<HarvestTile*>();
+	for (int i = 0; i < numTypes; i++) {
+		for (int j = 0; j < numTypes; j++) {
+			if (i != j) {
+				// For each resource type i and each type j, add a new tile with three i and one j.
+				tiles->add(new HarvestTile(
+					new ResourceToken(static_cast<ResourceType>(i)),
+					new ResourceToken(static_cast<ResourceType>(i)),
+					new ResourceToken(static_cast<ResourceType>(i)),
+					new ResourceToken(static_cast<ResourceType>(j))
+				));
+				// For each resource type i and each type j, add a new tile with two i and two j.
+				tiles->add(new HarvestTile(
+					new ResourceToken(static_cast<ResourceType>(i)),
+					new ResourceToken(static_cast<ResourceType>(j)),
+					new ResourceToken(static_cast<ResourceType>(i)),
+					new ResourceToken(static_cast<ResourceType>(j))
+				));
+			}
+		}
+	}
+	for (int i = 0; i < numTypes; i++) {
+		for (int j = 0; j < numTypes; j++) {
+			for (int k = 0; k < numTypes; k++) {
+				if (!(i == j || i == k || j == k)) {
+					// For each resource type i, each type j, and each type k, add a new tile
+					// with two i (on the first column), one j, and one k.
+					tiles->add(new HarvestTile(
+						new ResourceToken(static_cast<ResourceType>(i)),
+						new ResourceToken(static_cast<ResourceType>(j)),
+						new ResourceToken(static_cast<ResourceType>(i)),
+						new ResourceToken(static_cast<ResourceType>(k))
+					));
+					if (j < k) {
+						// For each resource type i, each type j, and each type k, add a new tile
+						// with two i (on the main diagonal), one j, and one k.
+						tiles->add(new HarvestTile(
+							new ResourceToken(static_cast<ResourceType>(i)),
+							new ResourceToken(static_cast<ResourceType>(j)),
+							new ResourceToken(static_cast<ResourceType>(k)),
+							new ResourceToken(static_cast<ResourceType>(i))
+						));
+					}
+				}
+			}
+		}
+	}
+	return tiles;
+}
+
+Deck<Building*>* buildingDeck() {
+	Deck<Building*>* buildings = new Deck<Building*>();
+	for (int i = 0; i < VGMap::HEIGHT; i++) {
+		for (int j = 0; j < VGMap::HEIGHT; j++) {
+			for (int k = 0; k < TokenGraph::NUM_TYPES; k++) {
+				BuildingType type = static_cast<BuildingType>(k);
+				buildings->add(new Building(type, i + 1));
+			}
+		}
+	}
+	return buildings;
 }
