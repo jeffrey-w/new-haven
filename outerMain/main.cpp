7a940d7d7d99a02f9502e2cd80bb11bd5b67bf50
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-22 20:13:11 -0400

    modify main(); include crtdbg

--- outerMain/main.cpp
+++ outerMain/main.cpp
@@ -1,3 +1,6 @@
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
 #include "Game.h"
 #include "test/Drivers.h"
 
@@ -10,5 +13,6 @@ int main() {
     testResources();
     testScoringFacilities();
     testGameStart();
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     return 0;
 }
