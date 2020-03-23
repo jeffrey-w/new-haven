f9a94b51fbee54a8a20f5c90090b3d6c19ccc3d1
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-22 20:48:17 -0400

    remove isTokenized()

--- outerMain/pieces/AbstractPiece.h
+++ outerMain/pieces/AbstractPiece.h
@@ -6,9 +6,7 @@
 class AbstractPiece {
 
 public:
-
-	// Returns true iff this AbstractPiece has been completely tokenized.
-	virtual bool isTokenized() const = 0;
+	
 	// Iteratively returns the tokens that constitute this AbstractPiece. Throws an exception if
 	// this Piece has already been tokenized.
 	virtual AbstractToken* tokenize() = 0;
