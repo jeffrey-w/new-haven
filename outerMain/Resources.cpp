e37065972f21d1cd1ddc8e177d258b12c6f9f158
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-27 23:56:20 -0400

    modify operator<<

--- outerMain/Resources.cpp
+++ outerMain/Resources.cpp
@@ -87,7 +87,8 @@ void HarvestTileHand::display() const {
 
 std::ostream& operator<<(std::ostream& stream, const HarvestTileHand& hand) {
 	stream << "1\t\t2\t\tShipment\n";
-	return HarvestTile::printHand(stream, *hand.one, *hand.two, *hand.shipment);
+	HarvestTile::printHand(stream, *hand.one, *hand.two, *hand.shipment);
+	return stream;
 }
 
 BuildingHand::BuildingHand() {
