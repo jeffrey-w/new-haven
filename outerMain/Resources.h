6a6327c899c20e93dbd47760a76de76c501ff124
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-02 03:28:25 -0400

    add parameterized constructor; modify insert()

--- outerMain/Resources.h
+++ outerMain/Resources.h
@@ -134,13 +134,15 @@ public:
 
 	// Constructs a new HarvestTileHand object.
 	HarvestTileHand();
+	// Constructs a new HarvestTileHand object with the specified shipment tile.
+	HarvestTileHand(HarvestTile*);
 	// Constructs a new HarvestTileHand object with the same contents as the specified Hand.
 	HarvestTileHand(const HarvestTileHand&);
 	// Destroys this HarvestTileHand.
 	~HarvestTileHand();
 	// Adds the specified HarvestTile to this HarvestTileHand. Throws an exception if this
 	// HarvestTileHand is full.
-	void insert(HarvestTile*, bool);
+	void insert(HarvestTile*);
 	// Returns the HarvestTile selected by a Player. Throws an exception if the specified selection
 	// is not between one and two inclusive, or the specified selection does not exit.
 	HarvestTile* select(int);
