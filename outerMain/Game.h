2f8b8bea7af98612c0089dc2cda8d78c667975da
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-02 03:24:57 -0400

    edit comments

--- outerMain/Game.h
+++ outerMain/Game.h
@@ -26,34 +26,33 @@ public:
 	size_t buildingsLeft() const;
 	// Adds a new Player with the specified id to this Game's Roster. Throws an exception if this
 	// Game's Roster is already full.
-	void addPlayer(uint64_t);
-	// Shuffles this Game's Decks, deals HarvestTiles and Buildings to each Player in this Game's
-	// Roster, and intitializes this Game's BuildingPool.
+	void addPlayer(long);
+	// Deals HarvestTiles and Buildings to each Player in this Game's Roster, and intitializes this
+	// Game's BuildingPool.
 	void setup();
-
-	// TODO need to throw exception if setup has not been called for each of the following:
-
 	// Rotates the selected HarvestTile in the current Player's HarvestTileHand. Throws an
-	// exception if the specified selection is not consistent with the current Player's hand.
+	// exception if the specified selection is not consistent with the current Player's hand, or if
+	// this Game has not been setup.
 	void rotateTile(int);
 	// Induces the current Player to place the selected HarvestTile onto this Game's GBMap at the
 	// specified square. Throws an exception if the specified tile selection is not consistent with
-	// the current Player's Hand; or if the specified square is not on this Game's GBMap or it is
-	// already occupied.
+	// the current Player's Hand; if the specified square is not on this Game's GBMap or it is
+	// already occupied; or if this Game has not been setup.
 	void playTile(int, std::pair<int, int>);
 	// Induces the current Player to place their shipment tile onto this Game's GBMap at the
 	// specified square. For one turn only, the specified square will contain four of the specified
 	// type. Throws an exception if the current Player has already played their shipment tile; if
-	// the specified square is not on this Game's GBMap or it is already occupied; or the specified
-	// type is not a valid ResourceType.
+	// the specified square is not on this Game's GBMap or it is already occupied; the specified
+	// type is not a valid ResourceType; or if this Game has not been setup.
 	void playShipment(std::pair<int, int>, int);
 	// Induces the current Player to build the selected Building on their VGMap at the specified
 	// circle. Throws an exception if any of the following conditions hold:
 	// (1) the selected building is not consistent with this Player's hand,
 	// (2) the specified circle is not on this Player's VGMap or it is already occupied,
 	// (3) the specified Building is not being placed adjacent to another of the same type, or
+	// (4) this Game has not been setup.
 	void playBuilding(int, std::pair<int, int>);
-	// Induces the current Player to yield the remaining resourers they gathered to the other
+	// Induces the current Player to cede the remaining resourers gathered this turn to the other
 	// Players of this game.
 	void yield();
 	// Induces the current Player to draw a Building from this Game's Deck. Throws an exception if
@@ -65,7 +64,9 @@ public:
 	void drawFromPool(int);
 	// Resets this Game's resource markers to zero, replenishes its BuildingPool, and induces the
 	// current Player to draw a new HarvestTile provided they did not just play their shipment tile.
-	void endTurn();
+	// Throws an exception if the current Player did not just play their shipment tile despite
+	// being indicated otherwise, or if this Game has not been setup.
+	void endTurn(bool);
 	// Writes this Game's GBMap to the standard output stream.
 	void displayBoard() const;
 	// Writes this Game's resource markers to the standard output stream.
