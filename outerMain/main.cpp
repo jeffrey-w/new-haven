20c120c4a1dc5ab1ffd15643ce98b7541c9d8598
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-03 17:18:54 -0400

    modify main()

--- outerMain/main.cpp
+++ outerMain/main.cpp
@@ -1,18 +1,24 @@
 #define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
+#include <iostream>
+#include <sstream>
 
-#include "Game.h"
+#include "Controller.h"
 #include "test/Drivers.h"
 
 int main() {
-    testGBMap();
-    testVGMap();
-    testGBMapLoader();
-    testVGMapLoader();
-    testPlayer();
-    testResources();
-    testScoringFacilities();
-    testGameStart();
+    //testGBMap();
+    //testVGMap();
+    //testGBMapLoader();
+    //testVGMapLoader();
+    //testPlayer();
+    //testResources();
+    //testScoringFacilities();
+    //testGameStart();
+    Controller c;
+    c.initGame();
+    c.inputIDs();
+    c.loop();
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     return 0;
 }
