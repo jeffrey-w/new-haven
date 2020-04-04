60dd0886a66d0bd682df99769a46d275c4d81279
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-03 22:13:41 -0400

    modify main()

--- outerMain/main.cpp
+++ outerMain/main.cpp
@@ -4,19 +4,29 @@
 #include "Controller.h"
 #include "test/Drivers.h"
 
-int main() {
-    testGBMap();
-    testVGMap();
-    testGBMapLoader();
-    testVGMapLoader();
-    testPlayer();
-    testResources();
-    testScoringFacilities();
-    testGameStart();
-    Controller c;
-    c.initGame();
-    c.inputIDs();
-    c.loop();
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-    return 0;
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        if (!std::strcmp("-t", argv[1])) {
+            testGBMap();
+            testVGMap();
+            testGBMapLoader();
+            testVGMapLoader();
+            testPlayer();
+            testResources();
+            testScoringFacilities();
+        }
+        else {
+            std::cerr << "Usage: outerMain <-t>\n";
+            std::cerr << "t: use this flag to run tests.";
+            return 1;
+        }
+    }
+    else {
+        Controller c;
+        c.initGame();
+        c.inputIDs();
+        c.loop();
+        _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+        return 0;
+    }
 }
