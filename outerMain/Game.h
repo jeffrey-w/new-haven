c8724c8cf899e89cd369619db51942ee59dfc204
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-11 13:13:32 -0400

    add ensureSetup(); improve documentation

--- outerMain/Game.h
+++ outerMain/Game.h
@@ -22,62 +22,66 @@ public:
 	~Game();
 	// Returns the number of Players playing this Game.
 	int numPlayers() const;
-	// Returns the ID of the current Player.
+	// Returns the ID of the current Player. Throws an exception if this Game has no Players.
 	long nextID() const;
 	// Returns true iff the current Player is able to build on their VGMap with the resources
-	// available.
+	// available. Throws an exception if this Game has no Players.
 	bool canPlay() const;
 	// Returns the number of resource types that were completely spent during the previous turn of
 	// this Game.
 	int exhausted() const;
 	// Returns true iff this Game's GBMap has only one unoccupied square.
 	int gameOver() const;
-	// Returns a list of the winning Player(s) IDs.
+	// Returns a list of the winning Player(s) IDs. Throws an exception if this Game has no Players.
 	std::list<long> winners() const;
-	// Returns the score of the winner of this Game.
+	// Returns the score of the winner of this Game. Throws an exception if this Game has no
+	// Players.
 	int highscore() const;
-	// Returns the number of Buildings that the winner of this Game failed to play.
+	// Returns the number of Buildings that the winner of this Game failed to play. Throws an
+	// exception if this Game has no Players.
 	int buidlingsLeft() const;
-	// Returns the number of Buildings the the winner of this Game played.
+	// Returns the number of Buildings the the winner of this Game played. Throws an exception if
+	// this Game has no Players.
 	int buildingsPlayed() const;
 	// Adds a new Player with the specified id to this Game's Roster. Throws an exception if this
-	// Game's Roster is already full.
+	// Game's Roster is already full, or if the specified id is invalid or already taken.
 	void addPlayer(long);
 	// Rotates the selected HarvestTile in the current Player's HarvestTileHand 90 degrees
 	// counterclockwise. Throws an exception if the specified selection is not consistent with the
-	// current Player's hand, or if this Game has not been setup.
+	// current Player's Hand, or if this Game has not started.
 	void rotateTile(int);
 	// Induces the current Player to place the selected HarvestTile onto this Game's GBMap at the
 	// specified square. Throws an exception if the specified tile selection is not consistent with
 	// the current Player's Hand; if the specified square is not on this Game's GBMap or it is
-	// already occupied; or if this Game has not been setup.
+	// already occupied; or if this Game has not started.
 	void playTile(int, std::pair<int, int>);
 	// Induces the current Player to place their shipment tile onto this Game's GBMap at the
 	// specified square. For one turn only, the specified square will contain four of the specified
 	// type. Throws an exception if the current Player has already played their shipment tile; if
 	// the specified square is not on this Game's GBMap or it is already occupied; the specified
-	// type is not a valid ResourceType; or if this Game has not been setup.
+	// type is not a valid ResourceType; or if this Game has not started.
 	void playShipment(std::pair<int, int>, int);
 	// Induces the current Player to build the selected Building on their VGMap at the specified
 	// circle. Throws an exception if any of the following conditions hold:
 	// (1) the selected building is not consistent with this Player's hand,
 	// (2) the specified circle is not on this Player's VGMap or it is already occupied,
 	// (3) the specified Building is not being placed adjacent to another of the same type, or
-	// (4) this Game has not been setup.
+	// (4) this Game has not started.
 	void playBuilding(int, std::pair<int, int>);
 	// Induces the current Player to cede the remaining resourers gathered this turn to the other
-	// Players of this game.
+	// Players of this game. Throws an exception if this Game has not started.
 	void yield();
 	// Induces the current Player to draw a Building from this Game's Deck. Throws an exception if
-	// this Game's Deck is empty.
+	// this Game's Deck is empty, or if this Game has not started.
 	void drawFromDeck();
 	// Induces the current Player to select a Building from this Game's BuildingPool. Throws an
 	// exception if the specified selection is out of range, or if it specifies a non-existant
-	// building.
+	// building, or if this Game has not started.
 	void drawFromPool(int);
 	// Resets this Game's resource markers to zero, replenishes its BuildingPool, and induces the
 	// current Player to draw a new HarvestTile if specified.Throws an exception if the current
-	// Player draws another HarvestTile but cannot hold it in their Hand.
+	// Player draws another HarvestTile but cannot hold it in their Hand, or if this Game has not
+	// started.
 	void endTurn(bool);
 	// Writes this Game's GBMap to the standard output stream.
 	void displayBoard() const;
@@ -85,13 +89,16 @@ public:
 	// types occupying the specified square. Throws an exception if the specified type is invalid
 	// or if the specified square is not on this Games's GBMap.
 	void displayBoard(int, std::pair<int, int>) const;
-	// Writes the current Player's HarvestTileHand to the standard output stream.
+	// Writes the current Player's HarvestTileHand to the standard output stream. Throws an
+	// exception if this Game has not Players.
 	void displayTiles() const;
-	// Writes the current Player's VGMap to the standard output stream.
+	// Writes the current Player's VGMap to the standard output stream. Throws an exception if this
+	// Game has no Players.
 	void displayVillage() const;
 	// Writes this Game's resource markers to the standard output stream.
 	void displayResources() const;
-	// Writes the current Player's BuildingHand to the standard output stream.
+	// Writes the current Player's BuildingHand to the standard output stream. Throws an exception
+	// if this Game has no Players.
 	void displayBuildings() const;
 	// Writes this Game's BuildingPool to the standard output stream.
 	void displayPool() const;
@@ -107,6 +114,7 @@ private:
 
 	bool atCapacity() const;
 	void setup();
+	void ensureSetup();
 
 };
 
