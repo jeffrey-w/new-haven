8ed138a2d4f8d1db6cf08c5160d9de09fbb1de86
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-14 21:57:04 -0400

    add toString()

--- outerMain/observers/Observable.h
+++ outerMain/observers/Observable.h
@@ -1,12 +1,21 @@
 #pragma once
 
 #include <set>
+#include <sstream>
+#include <string>
 
 #include "Observer.h"
 
 class Observable {
 
 public:
+
+	template <class T>
+	static std::string* toString(T& obj) {
+		std::stringstream stream;
+		stream << obj;
+		return new std::string(stream.str());
+	}
 
 	Observable();
 	~Observable();
