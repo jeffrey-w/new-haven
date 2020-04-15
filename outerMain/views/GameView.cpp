7ac556c8bdc828e2e6890851c52e55afcce7126a
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-15 19:16:39 -0400

    implement showBoard(), showResources(), showPool(), showTiles(), showBuildings(), and showVillage()

--- outerMain/views/GameView.cpp
+++ outerMain/views/GameView.cpp
@@ -1,1 +1,25 @@
 #include "GameView.h"
+
+void GameView::showBoard() const {
+	board->show();
+}
+
+void GameView::showResources() const {
+	resources->show();
+}
+
+void GameView::showPool() const {
+	pool->show();
+}
+
+void GameView::showTiles() const {
+	players->showTiles();
+}
+
+void GameView::showBuildings() const {
+	players->showBuildings();
+}
+
+void GameView::showVillage() const {
+	players->showVillage();
+}
