7946f6f71d6d1ff623117dbf25a8a4051754fd24
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-19 19:39:39 -0400

    modify constructor

--- outerMain/views/PlayerView.cpp
+++ outerMain/views/PlayerView.cpp
@@ -1,6 +1,9 @@
 #include "PlayerView.h"
 
 PlayerView::PlayerView(HarvestTileHand* tiles, BuildingHand* buildings, VGMap* village) {
+	if (!(tiles && buildings && village)) {
+		throw std::invalid_argument("Cannot observe the null subject.");
+	}
 	this->tiles = new View(tiles);
 	this->buildings = new View(buildings);
 	this->village = new View(village);
