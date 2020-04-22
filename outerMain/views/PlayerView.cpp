abc3204d4e99738687d51bb6facde62ad3cc33f1
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-22 18:43:25 -0400

    modify calls to View::View()

--- outerMain/views/PlayerView.cpp
+++ outerMain/views/PlayerView.cpp
@@ -1,12 +1,14 @@
 #include "PlayerView.h"
 
+using std::string;
+
 PlayerView::PlayerView(HarvestTileHand* tiles, BuildingHand* buildings, VGMap* village) {
 	if (!(tiles && buildings && village)) {
 		throw std::invalid_argument("Cannot observe the null subject.");
 	}
-	this->tiles = new View(tiles);
-	this->buildings = new View(buildings);
-	this->village = new View(village);
+	this->tiles = new View(tiles, new string("Tiles"));
+	this->buildings = new View(buildings, new string("Buildings"));
+	this->village = new View(village, new string("Village Board"));
 }
 
 PlayerView::~PlayerView() {
