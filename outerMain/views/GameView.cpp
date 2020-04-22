2ed44a7b0663f857f5303f84e8895c56e0c20738
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-22 18:42:16 -0400

    modify calls to View::View() and View::show()

--- outerMain/views/GameView.cpp
+++ outerMain/views/GameView.cpp
@@ -13,19 +13,19 @@ GameView::~GameView() {
 }
 
 void GameView::addStats(Roster* players) {
-	stats = new View(validateObservable(players));
+	stats = new View(validateObservable(players), new string("Game Stats"));
 }
 
 void GameView::addBoard(GBMap* board) {
-	this->board = new View(validateObservable(board));
+	this->board = new View(validateObservable(board), new string("Game Board"));
 }
 
 void GameView::addResources(GatherFacility* resources) {
-	this->resources = new View(validateObservable(resources));
+	this->resources = new View(validateObservable(resources), new string("Resources"));
 }
 
 void GameView::addPool(BuildingPool* pool) {
-	this->pool = new View(validateObservable(pool));
+	this->pool = new View(validateObservable(pool), new string("Building Pool"));
 }
 
 void GameView::addPlayers(Roster* players) {
@@ -36,51 +36,44 @@ void GameView::rotate() {
 	players->rotate();
 }
 
-void GameView::showStats(const string& header) const {
+void GameView::showStats(const string* header) const {
 	if (stats) {
-		std::cout << header << '\n';
-		stats->show();
+		stats->show(header);
 	}
 }
 
 void GameView::showBoard() const {
 	if (board) {
-		std::cout << "Game Board\n";
 		board->show();
 	}
 }
 
 void GameView::showResources() const {
 	if (resources) {
-		std::cout << "Resources\n";
 		resources->show();
 	}
 }
 
 void GameView::showPool() const {
 	if (pool) {
-		std::cout << "Building Pool\n";
 		pool->show();
 	}
 }
 
 void GameView::showTiles() const {
 	if (players) {
-		std::cout << "Harvest Tiles\n";
 		players->showTiles();
 	}
 }
 
 void GameView::showBuildings() const {
 	if (players) {
-		std::cout << "Buildings\n";
 		players->showBuildings();
 	}
 }
 
 void GameView::showVillage() const {
 	if (players) {
-		std::cout << "Village Board\n";
 		players->showVillage();
 	}
 }
