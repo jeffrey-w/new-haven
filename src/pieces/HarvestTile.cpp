f3c4bdeb0c17a38576b4cff6f55d89d5543d763d
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-08-31 21:25:54 -0400

    modify constructors, rotate and tokenize; add randomOrientation; remove I/O methods

--- src/pieces/HarvestTile.cpp
+++ src/pieces/HarvestTile.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
 #include "HarvestTile.h"
+#include "../util/Random.h"
 
 using std::vector;
 
 std::string HarvestTile::INVALID_ORIENTATION =
         "Tile must have an orientation between 0 and " + std::to_string(HarvestTile::NUM_RESOURCES - 1) + ".";
 
-HarvestTile::HarvestTile() : HarvestTile(0) {
+HarvestTile::HarvestTile() : HarvestTile(randomOrientation()) {
     for (int i = 0; i < NUM_RESOURCES; i++) {
         // Default constructor returns a random ResourceToken.
         resources->push_back(new ResourceToken());
@@ -15,7 +16,7 @@ HarvestTile::HarvestTile() : HarvestTile(0) {
 }
 
 HarvestTile::HarvestTile(ResourceToken* one, ResourceToken* two, ResourceToken* three, ResourceToken* four) :
-        HarvestTile(0) {
+        HarvestTile(randomOrientation()) {
     resources->push_back(one);
     resources->push_back(two);
     resources->push_back(three);
@@ -38,6 +39,10 @@ HarvestTile::HarvestTile(int orientation) {
     resources = new vector<ResourceToken*>();
 }
 
+int HarvestTile::randomOrientation() {
+    return Random::next(0, NUM_RESOURCES - 1);
+}
+
 HarvestTile::~HarvestTile() {
     for (auto& resource : *resources) {
         delete resource;
@@ -47,51 +52,13 @@ HarvestTile::~HarvestTile() {
 }
 
 void HarvestTile::rotate() {
-    *current = ++(*current) & NUM_RESOURCES - 1;
+    (*current)++;
 }
 
 ResourceToken* HarvestTile::tokenize() {
-    ResourceToken* returnToken = (*resources)[*current];
-    (*resources)[(*current)++] = nullptr;
     // Avoid an expensive division op.
     *current &= NUM_RESOURCES - 1;
-    return returnToken;
-}
-
-void HarvestTile::printHand(std::ostream& stream, const HarvestTile& one, const HarvestTile& two, bool shipment) {
-    one.printHalf(stream, *one.current);
-    two.printHalf(stream, *two.current);
-    if (shipment) {
-        stream << "-- | --";
-    }
-    stream << '\n';
-    one.printHalf(stream, (*one.current + NUM_RESOURCES - 1) & NUM_RESOURCES - 1, true);
-    two.printHalf(stream, (*two.current + NUM_RESOURCES - 1) & NUM_RESOURCES - 1, true);
-    if (shipment) {
-        stream << "-- | --";
-    }
-    stream << '\n';
-}
-
-std::ostream& operator<<(std::ostream& stream, const HarvestTile& tile) {
-    tile.printHalf(stream, *tile.current);
-    tile.printHalf(stream, (*tile.current + HarvestTile::NUM_RESOURCES - 1) & HarvestTile::NUM_RESOURCES - 1, true);
-    return std::cout << '\n';
-}
-
-void HarvestTile::printHalf(std::ostream& stream, int from, bool countdown) const {
-    for (int i = 0; i<NUM_RESOURCES> > 1; i++) {
-        stream << *(*resources)[from];
-        if (countdown) {
-            from--;
-        } else {
-            from++;
-        }
-        from &= NUM_RESOURCES - 1;
-        if (i & 1) {
-            stream << "\t\t";
-        } else {
-            stream << " | ";
-        }
-    }
+    ResourceToken* token = (*resources)[*current];
+    (*resources)[(*current)++] = nullptr;
+    return token;
 }
