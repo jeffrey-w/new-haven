daae322867cc9d28b7136da57e0d05d432c6f77c
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-09-14 20:40:25 -0400

    allocate data members on the stack; modify sort and toString

--- src/players/Roster.cpp
+++ src/players/Roster.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <sstream>
 
 #include "Roster.h"
@@ -7,51 +8,44 @@ using std::list;
 using std::map;
 using std::string;
 
-Roster::Roster() {
-    ids = new deque<long>();
-    players = new map<long, Player*>();
-}
-
 Roster::Roster(const Roster& other) : Roster() {
-    for (auto& id : *other.ids) {
-        ids->push_back(id);
+    for (auto& id : other.ids) {
+        ids.push_back(id);
     }
-    for (auto& entry : *other.players) {
-        (*players)[entry.first] = new Player(*entry.second);
+    for (auto& entry : other.players) {
+        players[entry.first] = new Player(*entry.second);
     }
 }
 
 Roster::~Roster() {
-    for (auto& entry : *players) {
+    for (auto& entry : players) {
         delete entry.second;
     }
-    delete ids;
-    delete players;
 }
 
 size_t Roster::getSize() const {
-    return players->size();
+    return players.size();
 }
 
 long Roster::nextID() const {
-    if (ids->empty()) {
+    if (ids.empty()) {
         throw std::runtime_error("Roster is empty.");
     }
-    return ids->front();
+    return ids.front();
 }
 
 void Roster::add(long id, Player* player) {
     if (id < 1) {
         throw std::invalid_argument("ID must be positive.");
     }
-    if (players->count(id)) {
+    if (players.count(id)) {
         throw std::invalid_argument("ID already exists.");
     }
     if (!player) {
         throw std::invalid_argument("Cannot add the null player.");
     }
-    ids->push_back(id);
-    (*players)[id] = player;
+    ids.push_back(id);
+    players[id] = player;
 }
 
 Player* Roster::peek() {
@@ -63,32 +57,32 @@ Player* Roster::next() {
 }
 
 Player* Roster::front(bool rotate) {
-    if (ids->empty()) {
+    if (ids.empty()) {
         throw std::runtime_error("Roster is empty.");
     }
-    long id = ids->front();
+    long id = ids.front();
     if (rotate) {
-        ids->pop_front();
-        ids->push_back(id);
+        ids.pop_front();
+        ids.push_back(id);
     }
-    return (*players)[id];
+    return players[id];
 }
 
 void Roster::sort() {
-    std::sort(ids->begin(), ids->end());
+    std::rotate(ids.begin(), std::min_element(ids.begin(), ids.end()), ids.end());
 }
 
 void Roster::deal(Deck<HarvestTile*>* tiles, Deck<Building*>* buildings) {
     if (!(tiles && buildings)) {
         throw std::invalid_argument("Cannot deal from null deck(s).");
     }
-    for (int i = 0; i < TILE_HAND_SIZE; i++) {
-        for (auto& entry : *players) {
+    for (int i = 0; i < HarvestTileHand::SIZE - 1; i++) {
+        for (auto& entry : players) {
             entry.second->drawTile(tiles);
         }
     }
     for (int i = 0; i < VGMap::HEIGHT; i++) {
-        for (auto& entry : *players) {
+        for (auto& entry : players) {
             entry.second->drawBuilding(buildings);
         }
     }
@@ -96,19 +90,27 @@ void Roster::deal(Deck<HarvestTile*>* tiles, Deck<Building*>* buildings) {
 
 string Roster::toString() const {
     std::ostringstream stream;
+    stream << "┌───────────┬───────────┬───────────┬───────────┐\n";
+    stream << "│ Player    │ Villagers │ Built     │ Unbuilt   │\n";
     for (auto& entry : invert()) {
-        stream << "Player " << entry.second << '\n';
-        stream << "villagers: " << entry.first.getVillagers();
-        stream << " built: " << entry.first.getBuilt();
-        stream << " unbuilt: " << entry.first.getUnbuilt() << '\n';
+    stream << "├───────────┼───────────┼───────────┼───────────┤\n";
+        stream << "│         ";
+        stream << entry.second;
+        stream << " │         ";
+        stream << entry.first.getVillagers();
+        stream << " │         ";
+        stream << entry.first.getBuilt();
+        stream << " │         ";
+        stream << entry.first.getUnbuilt();
+        stream << " │\n";
     }
-    stream << '\n';
+    stream << "└───────────┴───────────┴───────────┴───────────┘\n";
     return stream.str();
 }
 
 std::multimap<const Player, long> Roster::invert() const {
     std::multimap<const Player, long> sorted;
-    for (auto& entry : *players) {
+    for (auto& entry : players) {
         sorted.insert({*entry.second, entry.first});
     }
     return sorted;
@@ -116,7 +118,7 @@ std::multimap<const Player, long> Roster::invert() const {
 
 RosterView* rosterView(Roster* roster) {
     RosterView* view = new RosterView();
-    for (auto& entry : *roster->players) {
+    for (auto& entry : roster->players) {
         view->addView(playerView(entry.second));
     }
     return view;
