1139c135578af0eb5655a5bdd40f8687172becac
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-09-18 16:54:44 -0400

    formatting

--- src/views/GameView.h
+++ src/views/GameView.h
@@ -6,6 +6,7 @@
 
 // Provides Views for several New Haven objects.
 class GameView {
+
   public:
     // Constructs a new GameView object.
     GameView() = default;
@@ -13,25 +14,20 @@ class GameView {
     GameView(const GameView&) = delete;
     // Destroys this GameView.
     ~GameView();
-    // Registers an Observer with the specified Roster. Throws an exception if the specified Roster
-    // is null.
+    // Registers an Observer with the specified Roster. Throws an exception if the specified Roster is null.
     void addStats(Roster*);
-    // Registers an Observer with the specified GBMap. Throws an exception if the specified GBMap
-    // is null.
+    // Registers an Observer with the specified GBMap. Throws an exception if the specified GBMap is null.
     void addBoard(GBMap*);
-    // Registers an Observer with the specified GatherFacility. Throws an exception if the
-    // specified GatherFacility is null.
+    // Registers an Observer with the specified GatherFacility. Throws an exception if the specified GatherFacility is null.
     void addResources(GatherFacility*);
-    // Registers an Observer with the specified BuildingPool. Throws an exception if the specified
-    // BuildingPool is null.
+    // Registers an Observer with the specified BuildingPool. Throws an exception if the specified BuildingPool is null.
     void addPool(BuildingPool*);
-    // Registers Observers with the Players in the specified Roster. Throws an exception if the
-    // specified Roster is null.
+    // Registers Observers with the Players in the specified Roster. Throws an exception if the specified Roster is null.
     void addPlayers(Roster*);
     // Selects the next Player to observe.
     void rotate();
     // Displays statistics for every Player Observed by this GameView with the specified header.
-    void showStats(const std::string*) const;
+    void showStats(const std::string&) const;
     // Displays the GBMap Observed by this GameView.
     void showBoard() const;
     // Displays the GatherFacility observed by this GameView.
