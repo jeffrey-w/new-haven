eefea703e9f68a02865653c3a58f8926de1f975f
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-09-18 16:56:40 -0400

    allocate header on the stack

--- src/views/View.h
+++ src/views/View.h
@@ -10,17 +10,15 @@ class View : public Observer {
   public:
     // Constructs a new View object and registers it with the specified Observable. Throws an
     // exception if the specified Observable is null.
-    View(Observable*, std::string*);
+    View(Observable*, const std::string&);
     // Supress copy constructor.
     View(const View&) = delete;
-    // Destroys this View.
-    ~View();
     // Displays the subject of this View.
-    void show(const std::string* = nullptr) const;
+    void show(const std::string& = "") const;
     // Modifies the state of this View to reflect changes to its subject.
     void update() override;
 
   private:
     Observable* subject;
-    std::string* header;
+    std::string header;
 };
