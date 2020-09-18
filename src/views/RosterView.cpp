f91cfc2bf6c3246609399156f63ecdec67df44de
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-09-18 16:55:50 -0400

    allocate data members on the stack

--- src/views/RosterView.cpp
+++ src/views/RosterView.cpp
@@ -2,38 +2,34 @@
 
 using std::vector;
 
-RosterView::RosterView() {
-    current = new int(0);
-    players = new vector<PlayerView*>();
+RosterView::RosterView() : current(0) {
 }
 
 RosterView::~RosterView() {
-    for (auto& player : *players) {
+    for (auto& player : players) {
         delete player;
     }
-    delete current;
-    delete players;
 }
 
 void RosterView::addView(PlayerView* view) {
     if (!view) {
         throw std::invalid_argument("Cannot observe the null subject.");
     }
-    players->push_back(view);
+    players.push_back(view);
 }
 
 void RosterView::rotate() {
-    *current = ++(*current) % players->size();
+    current = ++current % players.size();
 }
 
 void RosterView::showTiles() const {
-    (*players)[*current]->showTiles();
+    players[current]->showTiles();
 }
 
 void RosterView::showBuildings() const {
-    (*players)[*current]->showBuildings();
+    players[current]->showBuildings();
 }
 
 void RosterView::showVillage() const {
-    (*players)[*current]->showVillage();
+    players[current]->showVillage();
 }
